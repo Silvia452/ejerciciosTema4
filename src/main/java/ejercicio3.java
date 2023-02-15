6dd6f1eea683e11eaf40eba7993e502678cc9ea3
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-02-15 17:28:37 +0100

    ejercicio 3 completado

--- src/main/java/ejercicio3.java
+++ src/main/java/ejercicio3.java
@@ -3,33 +3,31 @@
 import java.util.Scanner;
 
 public class ejercicio3 {
+    public static final Scanner sn = new Scanner(System.in);
     public static void main(String[] args) {
         mediaAritmetica();
-
     }
 
-    public static void mediaAritmetica(){
+    public static void mediaAritmetica() {
 
-        Scanner sc = new Scanner(System.in);
-        int[] valores = new int[100];
-        int suma = 0;
-        int i = 0;
-        int contadorNotasAltas = 0;
-        while (true) {
-            System.out.println("¿Nota(-1 para terminar)? ");
-            valores[i] = sc.nextInt();
-            if (valores[i] == -1) {
-                break;
+        double nota = 0;
+        double suma = 0;
+        int contador = 0;
+        int contador2 = 0;
+        double media = 0;
+        double porcentaje;
+        while (nota != -1) {
+            System.out.println("¿Nota (-1 para terminar)?: ");
+            nota = sn.nextDouble();
+            suma += nota;
+            contador++;
+            if (nota >= 10) {
+                contador2++;
             }
-            else if (valores[i] >= 10 || valores[i] >=20){
-                contadorNotasAltas++;
-            }
-
-            suma += valores[i];
-            i++;
         }
-        System.out.println("La media aritmética de los valores introducidos es: " + (suma / i));
-        System.out.println("Las notas que superan 10/20: " +  contadorNotasAltas*10 +" %" );
+        media = suma / contador;
+        porcentaje = contador2 * 100 / contador;
+        System.out.println(String.format("La media es %.2f (%.2f %% >= 10)", media, porcentaje));
     }
 
 }
